53320504ae603c2c534bf232b4640fc48d31d618
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-09-22 22:31:58 -0400

    :tada: streaming error handling + easy to see whats versions of important things are running

--- bin/scout-server-worker.js
+++ bin/scout-server-worker.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 
-require(require('path').resolve(__dirname, '../')).start();
+var chalk = require('chalk');
+var figures = require('figures');
+var format = require('util').format;
+var server = require('../');
+var versions = server.versions;
+
+server.start(function(err) {
+  if (!err) {
+    console.log('  ',
+      chalk.green(figures.tick),
+      ' scout-server',
+      chalk.bold(format('v%s', versions['scout-server'])),
+      'listening on', chalk.bold(server.config.get('listen')));
+    return;
+  }
+  if (err.code === 'EADDRINUSE') {
+    console.log('  ',
+      chalk.yellow(figures.warning),
+      ' Failed to start scout-server on', chalk.bold(server.config.get('listen')),
+      '. Are you running scout-server in another tab already?');
+    return;
+  }
+
+  console.log('  ',
+    chalk.bold.underline.red(figures.cross,
+      ' scout-server failed to start!\n'), err.stack || JSON.stringify(err));
+});
