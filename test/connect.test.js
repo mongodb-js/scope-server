cb4f3c7663288833b2ee23da52498068be1ab234
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-10-29 19:16:50 -0400

    :tada: nice groupings for func tests + more granularity

--- test/connect.test.js
+++ test/connect.test.js
@@ -1,26 +1,67 @@
+var _ = require('lodash');
 var assert = require('assert');
 var connect = require('mongodb-connection-model').connect;
 var format = require('util').format;
 var Instance = require('mongodb-instance-model');
-var fixtures = require('mongodb-connection-fixture').MATRIX;
+var Connection = require('mongodb-connection-model');
 
-var shouldGetInstanceDetails = function(db, done) {
-  assert(db);
-  Instance.fetch(db, done);
-};
+var fixtures = require('mongodb-connection-fixture').MATRIX.map(function(model) {
+  return new Connection(model);
+});
 
 if (fixtures.length > 0) {
   describe('functional #slow', function() {
-    fixtures.map(function(model) {
-      it(format('should connect to `%s`', model.name), function(done) {
-        this.slow(5000);
-        this.timeout(10000);
+    _.map(_.groupBy(fixtures, 'authentication'), function(models, authentication) {
+      describe(format('Using authentication `%s`', authentication), function() {
+        _.each(models, function(model) {
+          describe(model.name, function() {
+            var db;
+            it('should connect', function(done) {
+              if (process.env.dry) {
+                this.skip();
+                return;
+              }
+              this.slow(5000);
+              this.timeout(10000);
 
-        connect(model, function(err, _db) {
-          if (err) {
-            return done(err);
-          }
-          shouldGetInstanceDetails(_db, done);
+              connect(model, function(err, _db) {
+                if (err) {
+                  return done(err);
+                }
+                db = _db;
+                done();
+              });
+            });
+
+            it('should list collections', function(done) {
+              if (process.env.dry) {
+                this.skip();
+                return;
+              }
+              this.slow(5000);
+              this.timeout(10000);
+              assert(db);
+              Instance.fetch.getDatabaseCollections(db, done);
+            });
+
+            it('should get instance details', function(done) {
+              if (process.env.dry) {
+                this.skip();
+                return;
+              }
+
+              this.slow(5000);
+              this.timeout(10000);
+              assert(db);
+              Instance.fetch(db, done);
+            });
+
+            after(function() {
+              if (db) {
+                db.close();
+              }
+            });
+          });
         });
       });
     });
