508875f9f541194e730e26c67a46c5cdd6e9915e
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-11-23 14:25:30 -0500

    :bug: missed one last reference to old name

--- bin/mongodb-scope-server-worker.js
+++ bin/mongodb-scope-server-worker.js
@@ -11,20 +11,20 @@ server.start(function(err) {
   if (!err) {
     console.log('  ',
       chalk.green(figures.tick),
-      ' scout-server',
-      chalk.bold(format('v%s', versions['scout-server'])),
+      ' mongodb-scope-server',
+      chalk.bold(format('v%s', versions['mongodb-scope-server'])),
       'listening on', chalk.bold(server.config.get('listen')));
     return;
   }
   if (err.code === 'EADDRINUSE') {
     console.log('  ',
       chalk.yellow(figures.warning),
-      ' Failed to start scout-server on', chalk.bold(server.config.get('listen')),
-      '. Are you running scout-server in another tab already?');
+      ' Failed to start mongodb-scope-server on', chalk.bold(server.config.get('listen')),
+      '. Are you running mongodb-scope-server in another tab already?');
     return;
   }
 
   console.log('  ',
     chalk.bold.underline.red(figures.cross,
-      ' scout-server failed to start!\n'), err.stack || JSON.stringify(err));
+      ' mongodb-scope-server failed to start!\n'), err.stack || JSON.stringify(err));
 });
